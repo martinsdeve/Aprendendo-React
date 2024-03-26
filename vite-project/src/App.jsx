7bc840f2e629b333cffb957425cded09df5e50b6
martinsdeve <matheusgomesdeveloper38@gmail.com>
2024-03-26 00:09:31 -0500

    salvo

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Components/Header/Header'
 import Main from './Components/Main/Main'
@@ -11,11 +8,11 @@ import Footer from './Components/Footer/Footer'
 function App() {
 
   return (
-   <div>
-  <Header></Header>
-  <Main></Main>
-  <Footer></Footer>
-   </div>
+    <div>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </div>
 
   )
 }
